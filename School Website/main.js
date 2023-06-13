f3f687e5ccbd1e500126c6d06391f776d9e67ff0
Ashfaque <qureshiashfaque6@gmail.com>
2023-06-13 19:30:11 +0530

    FAQ section done

--- School Website/main.js
+++ School Website/main.js
@@ -3,3 +3,24 @@
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
 })
+
+
+//show/hide faq answers
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faqs => {
+    faqs.addEventListener('click', ()=>{
+        faqs.classList.toggle('open');
+
+        //change icon
+        // const icon = faq.querySelector('.faq__icon i');
+        // if(icon.className === 'fa-solid fa-plus'){
+        //     icon.className = "fa-solid fa-minus";
+        // }
+        // else {
+        //     icon.className = "fa-solid fa-plus";
+        // }
+
+    })
+})
