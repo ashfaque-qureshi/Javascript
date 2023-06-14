3badc92cf94af464488080d31796abd5dfd2f5aa
Ashfaque <qureshiashfaque6@gmail.com>
2023-06-14 21:08:37 +0530

    Final Done

--- School Website/main.js
+++ School Website/main.js
@@ -1,51 +1,28 @@
 //Change nav bar style on scroll
-
-window.addEventListener('scroll', ()=>{
-    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
-
-
 //show/hide faq answers
-
 const faqs = document.querySelectorAll('.faq');
-
 faqs.forEach(faqs => {
-    faqs.addEventListener('click', ()=>{
+    faqs.addEventListener('click', () => {
         faqs.classList.toggle('open');
-
-        //change icon
-        // const icon = faq.querySelector('.faq__icon i');
-        // if(icon.className === 'fa-solid fa-plus'){
-        //     icon.className = "fa-solid fa-minus";
-        // }
-        // else {
-        //     icon.className = "fa-solid fa-plus";
-        // }
-
+        
     })
 })
-
-
 //show/hide menu
-
 const menu = document.querySelector(".nav__menu");
 const menuBtn = document.querySelector("#open-menu-btn");
 const closeBtn = document.querySelector("#close-menu-btn");
-
-
-menuBtn.addEventListener('click', () =>{
-    menu.style.display="flex";
-    closeBtn.style.display="inline-block";
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
     menuBtn.style.display = "none";
 })
-
-
 //close nav menu
-
-const closenav = () =>{
-    menu.style.display="none";
-    closeBtn.style.display="none";
+const closenav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
     menuBtn.style.display = "inline-block";
 }
-
-closeBtn.addEventListener('click',closenav)
+closeBtn.addEventListener('click', closenav)
